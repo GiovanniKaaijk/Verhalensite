c90c5c96993950854c2c2726f4ddce67af18a6e1
giovanni <kaaijkg@gmail.com>
2018-12-05 12:01:41 +0100

    transavia verhalensite

---
+++ favorite.js
@@ -0,0 +1,49 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
+
+var trashBin = document.querySelectorAll('.fa-trash-alt');
+var i = 0;
+
+var row = '';
+
+
+var allButtons = document.querySelectorAll('.fa-arrow-alt-circle-down');
+var x = 0;
+
+for (x = 0; x < allButtons.length; x++) {
+    allButtons[x].addEventListener('click', spinning);
+
+}
+
+function spinning(e) {
+    var target = e.currentTarget;
+    var sibling = e.currentTarget.nextElementSibling;
+    sibling.classList.toggle('display');
+    target.classList.toggle('opacity');
+    setTimeout(function () {
+        sibling.classList.toggle('display');
+        target.classList.remove('fa-arrow-alt-circle-down');
+        target.classList.add('fa-check-circle');
+        target.classList.toggle('opacity');
+    }, 2000);
+}
+
+for (i = 0; i < trashBin.length; i++) {
+    console.log('trash');
+    trashBin[i].addEventListener('click', deleteRow);
+}
+
+
+
+
+function deleteRow(e) {
+    row = e.currentTarget.parentElement;
+    console.log(row);
+    row.classList.toggle('opacity');
+    row.classList.toggle('shrink');
+    setTimeout(function () {
+        row.classList.toggle('delete');
+    }, 500);
+}
--- javascript.js
+++ javascript.js
@@ -3,58 +3,19 @@
 /*eslint 'no-console':0*/
 
 var boekenIcon = document.querySelector('.boekenicon');
-var boekenLijst = document.querySelector('.boekenlijst');
 
-var downloadKnop = document.querySelector('.fa-arrow-alt-circle-down');
-var loadingBar = document.querySelector('.loadingbar');
-var percentage = document.querySelector('.loadingbar p');
-var laadPercentage;
-laadPercentage = 10;
+
+
+
 
 var hartje = document.querySelectorAll('.heart');
 
 
 
 
-function optellen() {
-    laadPercentage = laadPercentage + 10;
-    percentage.textContent = laadPercentage + ' / 100%';
-    if (laadPercentage >= 100) {
-        laadPercentage = 10;
-    }
-}
-
-function animatie() {
-    downloadKnop.classList.add('download');
-    loadingBar.classList.add('display');
-
-    //    setInterval(function optellen() {
-    //    laadPercentage = laadPercentage + 10;
-    //    percentage.textContent = laadPercentage + ' / 100%';
-    //    if (laadPercentage >= 100) {
-    //        laadPercentage = 0;
-    //        clearInterval(optellen);
-    //    }
-    //}, 500);
-
-    setInterval(optellen, 500);
-    setTimeout(function () {
-        downloadKnop.classList.remove('download');
-    }, 1000);
-    setTimeout(function () {
-        loadingBar.classList.remove('display');
-        downloadKnop.classList.remove('fa-arrow-alt-circle-down');
-        downloadKnop.classList.add('fa-check-circle');
-        clearInterval(optellen);
-    }, 5000);
-}
 
 
 
-boekenIcon.addEventListener('click', function () {
-    boekenLijst.classList.toggle('show');
-});
-downloadKnop.addEventListener('click', animatie);
 
 
 var i = 0;
